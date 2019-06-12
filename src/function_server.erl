a3b0b805ee1a3beaef682d912dfe364b1d8b9cfa
Daher Daher <daher928@campus.technion.ac.il>
2019-06-12 19:07:36 +0300

    Initial uploading from eclipse

---
+++ src/function_server.erl
@@ -0,0 +1,117 @@
+%% @author Daher
+%% @doc @todo Add description to function_server.
+
+
+-module(function_server).
+-behaviour(gen_server).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([start_link/1]).
+-export([stop/1]).
+-export([num_running_functions/1]).
+-export([calcFunction/4]).
+
+start_link(ServerName) ->
+	gen_server:start_link({local, ServerName}, ?MODULE, [], []).
+
+stop(ServerName) ->
+	gen_server:call({local, ServerName}, stop).
+
+num_running_functions(ServerName) ->
+	gen_server:call({local, ServerName}, numOfRunning).
+
+calcFunction(ServerName, Pid, Function, MsgRef) ->
+	gen_server:call({local, ServerName}, {calculate, Pid, Function, MsgRef}).
+
+
+%% ====================================================================
+%% Behavioural functions
+%% ====================================================================
+
+%% init/1
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:init-1">gen_server:init/1</a>
+-spec init(Args :: term()) -> Result when
+	Result :: {ok, State}
+			| {ok, State, Timeout}
+			| {ok, State, hibernate}
+			| {stop, Reason :: term()}
+			| ignore,
+	State :: term(),
+	Timeout :: non_neg_integer() | infinity.
+%% ====================================================================
+init([]) ->
+    {ok, 0}.
+
+
+%% handle_call/3
+handle_call(Request, From, State) ->
+    case Request of
+				numOfRunning -> {reply, State, State};
+				{calculate, Pid, Function, MsgRef} -> {reply, State+1, State+1}
+    end.
+
+
+%% handle_cast/2
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:handle_cast-2">gen_server:handle_cast/2</a>
+-spec handle_cast(Request :: term(), State :: term()) -> Result when
+	Result :: {noreply, NewState}
+			| {noreply, NewState, Timeout}
+			| {noreply, NewState, hibernate}
+			| {stop, Reason :: term(), NewState},
+	NewState :: term(),
+	Timeout :: non_neg_integer() | infinity.
+%% ====================================================================
+handle_cast(Msg, State) ->
+    {noreply, State}.
+
+
+%% handle_info/2
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:handle_info-2">gen_server:handle_info/2</a>
+-spec handle_info(Info :: timeout | term(), State :: term()) -> Result when
+	Result :: {noreply, NewState}
+			| {noreply, NewState, Timeout}
+			| {noreply, NewState, hibernate}
+			| {stop, Reason :: term(), NewState},
+	NewState :: term(),
+	Timeout :: non_neg_integer() | infinity.
+%% ====================================================================
+handle_info(Info, State) ->
+    {noreply, State}.
+
+
+%% terminate/2
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:terminate-2">gen_server:terminate/2</a>
+-spec terminate(Reason, State :: term()) -> Any :: term() when
+	Reason :: normal
+			| shutdown
+			| {shutdown, term()}
+			| term().
+%% ====================================================================
+terminate(Reason, State) ->
+    ok.
+
+
+%% code_change/3
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:code_change-3">gen_server:code_change/3</a>
+-spec code_change(OldVsn, State :: term(), Extra :: term()) -> Result when
+	Result :: {ok, NewState :: term()} | {error, Reason :: term()},
+	OldVsn :: Vsn | {down, Vsn},
+	Vsn :: term().
+%% ====================================================================
+code_change(OldVsn, State, Extra) ->
+    {ok, State}.
+
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+
